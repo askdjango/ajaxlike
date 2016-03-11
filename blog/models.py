b5f580a142fb664804c41f4cd7adf59afa381d56
Chinseok Lee <allieuslee@gmail.com>
2016-03-11 17:02:34 +0900

    blog.Post 모델 정의

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    like_count = models.PositiveIntegerField(default=0)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True)
