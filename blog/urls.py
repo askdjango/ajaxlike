efc03e36280c47162fd6444fdb8a52ae5cb00d5b
Chinseok Lee <allieuslee@gmail.com>
2016-03-11 17:40:22 +0900

    ajax post like 구현 (get 방식)

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^$', 'blog.views.post_list'),
     url(r'^(?P<pk>\d+)/$', 'blog.views.post_detail'),
+    url(r'^(?P<pk>\d+)/like/$', 'blog.views.post_like'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, render
 from .models import Post
 
@@ -14,3 +16,13 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {
         'post': post,
     })
+
+
+@login_required
+# @require_POST
+def post_like(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.like_users.add(request.user)
+    post.like_count = post.like_users.count()
+    post.save()
+    return JsonResponse({'ok': True, 'like_count': post.like_count}, safe=False)
